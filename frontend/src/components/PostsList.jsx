6590fcc9d0da5fab2fb77282a55b2f864183cde2
Sayed Rokibul Hossain <jaky1206@yandex.com>
2024-07-10 16:57:06 +0700

    hadling side effects

--- frontend/src/components/PostsList.jsx
+++ frontend/src/components/PostsList.jsx
@@ -1,44 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
-
 import classes from "./PostsList.module.css";
 
-function PostsList({ IsPosting, onStopPosting }) {
+function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
-  function addPostsHandler(postData) {
-    fetch('http://localhost:8080/posts', {
-      method: 'POST',
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
       body: JSON.stringify(postData),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
-    setPosts((exisitingPosts) => {
-      return [postData, ...exisitingPosts];
-    });
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
   return (
     <>
-      {IsPosting && (
+      {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} onAddPost={addPostsHandler} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
       {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
-            <Post key={post.body} author={post.author} body={post.body}></Post>
+            <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
       {posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
-          <h2>No posts found.</h2>
+          <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
         </div>
       )}
